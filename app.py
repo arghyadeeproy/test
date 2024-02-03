9f7f15869e499a38abd9e89746ebf7e5a888bb17
arghyadeeproy <143247778+arghyadeeproy@users.noreply.github.com>
2024-02-04 01:39:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import render_template, jsonify, Flask, redirect, url_for, request, make_response
+from flask import render_template, jsonify, Flask, request, make_response
 import os
 import io
 import numpy as np
 from PIL import Image
-import keras.utils as image
 from keras.models import model_from_json
 
 app = Flask(__name__)
@@ -16,9 +15,11 @@ SKIN_CLASSES = {
     4: 'Melanoma',
     5: 'Melanocytic Nevi',
     6: 'Vascular skin lesion'
-
 }
 
+# Set the absolute paths to the model files
+model_json_path = 'model.json'
+model_weights_path = 'model.h5'
 
 @app.route('/')
 def index():
@@ -42,10 +43,9 @@ def findMedicine(pred):
     elif pred == 5:
         return "fluorouracil"
     elif pred == 6:
-        return "fluorouracil"        
+        return "fluorouracil"
 
-
-@app.route('/detect', methods=['GET', 'POST'])
+@app.route('/detect', methods=['POST'])
 def detect():
     json_response = {}
     if request.method == 'POST':
@@ -59,43 +59,44 @@ def detect():
             }), 400)
 
         imagePil = Image.open(io.BytesIO(file.read()))
+
         # Save the image to a BytesIO object
         imageBytesIO = io.BytesIO()
         imagePil.save(imageBytesIO, format='JPEG')
         imageBytesIO.seek(0)
-        print("detected ")
-        path = imageBytesIO
-        j_file = open('model.json', 'r')
-        loaded_json_model = j_file.read()
-        j_file.close()
+
+        # Load model architecture from JSON
+        with open(model_json_path, 'r') as json_file:
+            loaded_json_model = json_file.read()
+
+        # Load model from JSON
         model = model_from_json(loaded_json_model)
-        model.load_weights('model.h5')
-        img = image.load_img(path, target_size=(224, 224))
-        img = np.array(img)
+
+        # Load model weights
+        model.load_weights(model_weights_path)
+
+        img = np.array(imagePil.resize((224, 224)))
         img = img.reshape((1, 224, 224, 3))
-        img = img/255
+        img = img / 255.0
+
         prediction = model.predict(img)
         pred = np.argmax(prediction)
         disease = SKIN_CLASSES[pred]
         accuracy = prediction[0][pred]
-        accuracy = round(accuracy*100, 2)
-        medicine=findMedicine(pred)
+        accuracy = round(accuracy * 100, 2)
+        medicine = findMedicine(pred)
 
         json_response = {
             "detected": False if pred == 2 else True,
             "disease": disease,
             "accuracy": accuracy,
-            "medicine" : medicine,
+            "medicine": medicine,
             "img_path": file.filename,
-
         }
 
         return make_response(jsonify(json_response), 200)
-
     else:
         return render_template('detect.html')
 
-
 if __name__ == "__main__":
-    app.run(host ="0.0.0.0", port=5000)
-
+    app.run(host="0.0.0.0", port=5000)
